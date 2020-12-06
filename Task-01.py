714c5dcc4a19748a0ef25ad8ccd72b380e32e7be
rtretyakov <rtretyakov@outlook.com>
2020-12-06 22:46:54 +1000

    task-01 completed added py-files

---
+++ Task-01.py
@@ -0,0 +1,24 @@
+# Третьяков Роман Викторович
+# Факультет Geek University Python-разработки
+# Основы языка Python
+# Урок 1
+# Задание 1
+
+var_int = 1
+var_float = 2.1
+var_str = 'Test string'
+var_bool = True
+
+print('var_int =', var_int, type(var_int))
+print('var_float =', var_float, type(var_float))
+print('var_str =', var_str, type(var_str))
+print('var_bool =', var_bool, type(var_bool))
+
+get_int = int(input('Введите целое число'))
+print('get_int = ', get_int, type(get_int))
+
+get_float = float(input('Введите число с плавающей запятой'))
+print('get_float = ', get_float, type(get_float))
+
+get_str = input('Введите строку')
+print('get_str = ', get_str, type(get_str))
